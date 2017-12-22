f289eece6b2cd14b32539b9586dd8503da337905
goodboy <>
2017-12-22 19:18:12 +0800

    更新使用说明

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # itc
 用python实现ipa打包，上传ipa到itunesconnect
+
+# 使用方法
+1, 配置conf.py的文件即可使用，具体需要配置什么参数，直接查看conf.py，文件中有参数注释
+2, 运行 python go.py
+
+# 注意事项
+1，确认配置的版本好能上传到itc
+2，最好通过APP-SPECIFIC PASSWORDS来设置发布账号密码
+
--- conf.py
+++ conf.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python
 # coding:utf-8
 
+# iTMSTransporter工具路径
+iTMSTransporter = "/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/itms/bin/iTMSTransporter"
+
+# 发布账号
 distribute_account = "xxx"
 
+# 发布账号密码
 distribute_pwd = "xxx"
 
-bundle_short_version_string = "1.3.0"
-
-bundle_version = "151"
-
-project_path = "/Users/goodboy/Desktop/mm/happytime"
-
-scheme = "HappyTime"
-
-configuration = "Release"
-
+# SKU 在itc上可以查看到这个值
 vendor_id = "FUNV20150118"
 
-provisioning_profile_name = "HappyTime_AppStore"
-
+# 苹果商店id  在itc上可以查看到这个值
 apple_id = "959458417"
 
+# 工程路径
+project_path = "/Users/goodboy/Desktop/mm/happytime"
+
+# scheme
+scheme = "HappyTime"
+
+# app字符串唯一表示
 bundle_identifier = "com.crossmirror.funv"
 
-iTMSTransporter = "/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/itms/bin/iTMSTransporter"
+# short version
+bundle_short_version_string = "1.3.0"
+
+# bundle version
+bundle_version = "151"
 
